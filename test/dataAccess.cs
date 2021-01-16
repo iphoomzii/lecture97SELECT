67b64ac0b8653ef98baac017d7210c30377d9276
Phoom <iphoomzii@gmail.com>
2021-01-16 16:45:10 +0700

    first commit

---
+++ test/dataAccess.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace test
+{
+    class dataAccess
+    {
+        public async static void InitializeDatabase()
+        {
+
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename=sample.db"))
+            {
+                db.Open();
+
+                String tableCommand = "CREATE TABLE IF NOT " +
+                    "EXISTS MyTable (Primary_Key INTEGER PRIMARY KEY, " +
+                    "Text_Entry NVARCHAR(2048) NULL)";
+
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteReader();
+            }
+        }
+
+        public async static void AddData(string inputText)
+        {
+            using (SqliteConnection db = new SqliteConnection($"Filename=sample.db"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                insertCommand.CommandText = "INSERT INTO MyTable VALUES(NULL,@Entry);";
+                insertCommand.Parameters.AddWithValue("@Entry", inputText);
+
+                insertCommand.ExecuteReader();
+                db.Close();
+            }
+        }
+
+        public static List<String> GetData()
+        {
+            List<String> entries = new List<string>();
+            using (SqliteConnection db = new SqliteConnection($"Filename=sample.db"))
+            {
+                db.Open();
+                SqliteCommand selectCommand = new SqliteCommand
+            ("SELECT Text_Entry from MyTable", db);
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+
+                while (query.Read())
+                {
+                    entries.Add(query.GetString(0));
+                }
+
+                db.Close();
+            }
+            return entries;
+        }
+    }
+}
